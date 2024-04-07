65cc19b12d77468393c52f364f48762735c9def9
paulius-sak <aspauliusas@gmail.com>
2024-04-07 22:20:01 +0300

    flight update

--- addFlight/addFlight.js
+++ addFlight/addFlight.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const addFlightForm = document.getElementById("add-flight-form");
+  const updateFlightForm = document.getElementById("update-flight-form");
 
   addFlightForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -9,7 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     createFlight(flightData, addFlightForm);
   });
+
+  updateFlightForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(updateFlightForm);
+    const flightData = Object.fromEntries(formData.entries());
+
+    updateFlight(flightData, updateFlightForm);
+  });
 });
+
+
+
+
 
 async function createFlight(flightData, addFlightForm) {
   try {
@@ -33,7 +47,37 @@ async function createFlight(flightData, addFlightForm) {
     addMessage.id = "add-message";
     addMessage.innerText = "Flight added successfully!";
     addFlightForm.appendChild(addMessage);
+
   } catch (error) {
     console.error("Error adding flight:", error);
+  }
+}
+
+async function updateFlight(flightData, updateFlightForm) {
+  try {
+    const response = await fetch(`http://localhost:3001/flights/${flightData.flightId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(flightData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update flight");
+    }
+
+    const responseData = await response.json();
+    console.log("Flight updated successfully:", responseData);
+
+    const updateMessage = document.createElement("p");
+    updateMessage.classList.add("update-message");
+    updateMessage.id = "update-message";
+    updateMessage.innerText = "Flight updated successfully!";
+    updateFlightForm.appendChild(updateMessage);
+
+    
+  } catch (error) {
+    console.error("Error updating flight:", error);
   }
 }
